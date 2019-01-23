d036053a4762410f37aea303db0275da72a15d2c
Nejdet Cetinkaya <nejdet@webulos.com>
2019-01-23 15:39:25 +0100

    windows wpf platform added

---
+++ wpf/RNZipArchive/RNZipArchive/RNZipArchiveModule.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.IO.Compression;
+using ReactNative.Bridge;
+using ReactNative.Modules.Core;
+using System.Threading.Tasks;
+
+namespace RNZipArchive
+{
+    public class RNZipArchiveModule: ReactContextNativeModuleBase
+    {
+        public RNZipArchiveModule(ReactContext reactContext)
+            : base(reactContext)
+        {
+        }
+
+        private RCTNativeAppEventEmitter _emitter;
+
+        public override string Name
+        {
+            get
+            {
+                return "RNZipArchive";
+            }
+        }
+
+        internal RCTNativeAppEventEmitter Emitter
+        {
+            get
+            {
+                if (_emitter == null)
+                {
+                    return Context.GetJavaScriptModule<RCTNativeAppEventEmitter>();
+                }
+
+                return _emitter;
+            }
+            set
+            {
+                _emitter = value;
+            }
+        }
+
+        [ReactMethod]
+        public async void unzip(string source, string target, IPromise promise)
+        {
+            try
+            {
+                await Task.Run(() => ZipFile.ExtractToDirectory(source, target) ).ConfigureAwait(false);
+                promise.Resolve(null);
+            }
+            catch (Exception ex)
+            {
+                promise.Reject(ex);
+            }
+        }
+
+        [ReactMethod]
+        public async void zip(string source, string target, IPromise promise)
+        {
+            try
+            {
+                await Task.Run(() => ZipFile.CreateFromDirectory(source, target)).ConfigureAwait(false);
+                promise.Resolve(null);
+            }
+            catch (Exception ex)
+            {
+                promise.Reject(ex);
+            }
+        }
+
+    }
+}
